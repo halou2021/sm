ba4d9cd3bb81b619c5651e68d69b1de18929ce00
halou2021 <95268228+halou2021@users.noreply.github.com>
2022-01-19 22:33:45 +0800

    Update timecard.js

--- sgmodule/time/timecard.js
+++ sgmodule/time/timecard.js
@@ -5,10 +5,11 @@ var tlist = {
   4: ["清明", "2022-04-05"],
   5: ["劳动", "2022-05-01"],
   6: ["端午", "2022-06-03"],
-  7: ["中秋", "2022-09-10"],
-  8: ["国庆", "2022-10-01"],
-  9: ["元旦", "2022-01-01"],
-  10: ["春节", "2022-01-22"]
+  7: ["七夕", "2022-08-04"],
+  8: ["中秋", "2022-09-10"],
+  9: ["国庆", "2022-10-01"],
+  10: ["元旦", "2023-01-01"],
+  11: ["春节", "2023-01-22"]
 };
 let tnow = new Date();
 let tnowf =
@@ -71,6 +72,7 @@ function datenotice() {
 }
 $done({
 title:"节假日倒计时",
-icon:"tortoise",
-content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+"天,"+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
+icon:"list.dash.header.rectangle",
+'icon-color': "#5AC8FA",
+content:tlist[nowlist][0]+"  :  "+today(tnumcount(nowlist))+"天\n"+tlist[Number(nowlist) + Number(1)][0] +"  :  "+ tnumcount(Number(nowlist) + Number(1))+ "天\n"+tlist[Number(nowlist) + Number(2)][0]+"  :  "+tnumcount(Number(nowlist) + Number(2))+"天"
 })
