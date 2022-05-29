211be36c41a77e02f5aeb7b2e907b617a29b6b2f
halou2021 <95268228+halou2021@users.noreply.github.com>
2022-05-29 12:06:34 +0800

    Update flyplane.js

--- sgmodule/flyplane.js
+++ sgmodule/flyplane.js
@@ -1,25 +1,24 @@
 /*
-模板来自@mieqq大佬（转大佬感谢：感谢@asukanana,感谢@congcong.）。
-
-感谢@mieqq大佬
-感谢@W cyber大佬的计算上的帮助。
-
-Surge配置参考注释，
+Surge配置参考注释，感谢@asukanana,感谢@congcong.
 
 示例↓↓↓ 
 ----------------------------------------
+
 [Script]
-Sub_info = type=generic,timeout=10,script-path= https://raw.githubusercontent.com/chaizia/Profiles/master/MySurge/sub_info_panel.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
+Sub_info = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info_panel.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
 
 [Panel]
-Sub_info = script-name=Sub_info,update-interval=86400
+Sub_info = script-name=Sub_info,update-interval=600
+
 ----------------------------------------
 
 先将带有流量信息的节点订阅链接encode，用encode后的链接替换"url="后面的[机场节点链接]
 
+（实在不会可以用这个捷径生成panel和脚本，https://www.icloud.com/shortcuts/3f24df391d594a73abd04ebdccd92584）
+
 可选参数 &reset_day，后面的数字替换成流量每月重置的日期，如1号就写1，8号就写8。如"&reset_day=8",不加该参数不显示流量重置信息。
 
-可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。
+可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。不希望显示到期信息也可以添加&expire=false取消显示。
 
 可选参数"title=xxx" 可以自定义标题。
 
@@ -29,27 +28,27 @@ Sub_info = script-name=Sub_info,update-interval=86400
 ----------------------------------------
 */
 
+let args = getArgs();
+
 (async () => {
-  let args = getArgs();
   let info = await getDataInfo(args.url);
   if (!info) $done();
   let resetDayLeft = getRmainingDays(parseInt(args["reset_day"]));
-  
+
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let proportion = used / total;
-  let content = [`已用 : ${toPercent(proportion)} | 剩余 : ${bytesToSize(total-used)}`];
+  let content = [`已用：${(used/total*100).toFixed(2)}%  |  剩余：${bytesToSize(total-used)}`];
+  //let content = [`剩余：${bytesToSize(total-used)}`]
 
   if (resetDayLeft) {
     content.push(`重置：剩余${resetDayLeft}天`);
   }
-
-    if (expire) {
+  if (expire && expire !== "false") {
     if (/^[\d.]+$/.test(expire)) expire *= 1000;
     content.push(`到期：${formatTime(expire)}`);
   }
-*/
+
   let now = new Date();
   let hour = now.getHours();
   let minutes = now.getMinutes();
@@ -57,8 +56,9 @@ Sub_info = script-name=Sub_info,update-interval=86400
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `${args.title} ` + `| 重置 ： ` + `${resetDayLeft} Days`,
+    title: `${args.title} | ${bytesToSize(total)} | ${hour}:${minutes}`,
     content: content.join("\n"),
+    //content: `剩余：${bytesToSize(total-used)} | ${resetDayLeft}天`,
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
   });
@@ -74,9 +74,10 @@ function getArgs() {
 }
 
 function getUserInfo(url) {
+  let method = args.method || "head";
   let request = { headers: { "User-Agent": "Quantumult%20X" }, url };
   return new Promise((resolve, reject) =>
-    $httpClient.get(request, (err, resp) => {
+    $httpClient[method](request, (err, resp) => {
       if (err != null) {
         reject(err);
         return;
@@ -137,7 +138,6 @@ function bytesToSize(bytes) {
   let k = 1024;
   sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   let i = Math.floor(Math.log(bytes) / Math.log(k));
-  if (i === 4 ) return (((bytes / Math.pow(k, i)))*1024).toFixed(2)+ " " + sizes[i-1];
   return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
 }
 
@@ -147,9 +147,4 @@ function formatTime(time) {
   let month = dateObj.getMonth() + 1;
   let day = dateObj.getDate();
   return year + "年" + month + "月" + day + "日";
-}
-
-function toPercent(proportion) {
-  const percent = Number(proportion*100).toFixed(2);
-  return percent + "%";
 }
